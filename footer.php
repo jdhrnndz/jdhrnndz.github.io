8fb25729fc4c456dc51b35802b90b94e972f27dc
jdhrnndz <johndeniellehernandez@gmail.com>
2014-05-29 14:43:24 +0800

    Removed unnecessary files

--- footer.php
+++
@@ -1,6 +0,0 @@
-		<footer>
-			<p>COPYRIGHT &copy Alpha Pichu Papi | Marathon Registration | <a href="contact-us.php">Contact Us</a></p>
-		</footer>
-		<?php mysqli_close($conn); ?>
-	</body>
-</html>
